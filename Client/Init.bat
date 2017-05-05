f578df52917c7324073e48ce72610f77b62bfe71
fdtteknik <teknik@fdt.se>
2017-05-05 12:32:10 +0200

    Fetch from master

--- Client/Init.bat
+++ Client/Init.bat
@@ -3,5 +3,5 @@
 SET PowerShellScriptPath=C:\FDT\KundSetup\Client\Setup.ps1
 echo %PowerShellScriptPath%
 mkdir C:\FDT\KundSetup\Client
-bitsadmin.exe /transfer "FDT SETUP" https://raw.githubusercontent.com/fdtteknik/KundSetup/Client-Server-XML/Client/Setup.ps1 %PowerShellScriptPath%
+bitsadmin.exe /transfer "FDT SETUP" https://raw.githubusercontent.com/fdtteknik/KundSetup/master/Client/Setup.ps1 %PowerShellScriptPath%
 PowerShell -NoProfile -noexit -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -noexit -ExecutionPolicy Unrestricted -File ""%PowerShellScriptPath%""' -Verb RunAs}";
