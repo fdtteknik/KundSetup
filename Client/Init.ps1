2ec12f5bfaadddb158f7e32343b87f1a10e13ccc
fdtteknik <teknik@fdt.se>
2017-05-29 22:06:13 +0200

    RDP setup fixes
    Added FDT.RDP reference file and made changes to the init.ps1 script
    creating the FDT.rdp file at the target

--- Client/Init.ps1
+++ Client/Init.ps1
@@ -255,7 +255,7 @@ function getBackofficeplatserBySeq($xmlBackofficeplatser, $seq) {
 
 function Create-RDP($dtyp, $seq, $name, $xml) {
    Write-Host "Creating RDP Shortcuts"
-   $resWidth = 1024
+   $resWidth = 1366
    $resHgt = 768
 
    $destination = $xml.RDS
@@ -278,31 +278,54 @@ $username = $dator.rdp
 #####################
 $hereString = @"
 
-audiomode:i:2
-authentication level:i:0
-autoreconnection enabled:i:1
-bitmapcachepersistenable:i:1
+use multimon:i:0
+session bpp:i:32
+winposstr:s:0,1,393,69,1193,669
 compression:i:1
-disable cursor setting:i:0
+keyboardhook:i:2
+audiocapturemode:i:0
+videoplaybackmode:i:1
+connection type:i:7
+networkautodetect:i:1
+bandwidthautodetect:i:1
+displayconnectionbar:i:1
+enableworkspacereconnect:i:0
+disable wallpaper:i:0
+allow font smoothing:i:0
+allow desktop composition:i:0
 disable full window drag:i:1
 disable menu anims:i:1
-disable themes:i:1
-disable wallpaper:i:1
-displayconnectionbar:i:1
-keyboardhook:i:2
-redirectclipboard:i:1
-redirectcomports:i:0
-redirectdrives:i:0
+disable themes:i:0
+disable cursor setting:i:0
+bitmapcachepersistenable:i:1
+audiomode:i:0
 redirectprinters:i:0
-redirectsmartcards:i:0
-session bpp:i:16
+redirectcomports:i:0
+redirectsmartcards:i:1
+redirectclipboard:i:1
+redirectposdevices:i:0
+autoreconnection enabled:i:1
+authentication level:i:2
 prompt for credentials:i:0
-promptcredentialonce:i:1
+negotiate security layer:i:1
+remoteapplicationmode:i:0
+alternate shell:s:
+shell working directory:s:
+gatewayhostname:s:
+gatewayusagemethod:i:4
+gatewaycredentialssource:i:4
+gatewayprofileusagemethod:i:0
+promptcredentialonce:i:0
+gatewaybrokeringtype:i:0
+use redirection server name:i:0
+rdgiskdcproxy:i:0
+kdcproxyname:s:
+drivestoredirect:s:
 "@
 
 $out = @()
 $out += "full address:s:" + $destination
-$out += "screen mode id:i:1"
+$out += "screen mode id:i:2"
 $out += "desktopwidth:i:" + $resWidth
 $out += "desktopheight:i:" + $resHgt
 $out += "username:s:" + $domain + "\" + $username
