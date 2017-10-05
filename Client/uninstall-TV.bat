5b3add4f6dbf6ce2fe0a1c5615b0b0179ca5a5b9
fdtteknik <teknik@fdt.se>
2017-10-05 15:56:20 +0200

    Created uninstall

---
+++ Client/uninstall-TV.bat
@@ -0,0 +1,1 @@
+mkdir C:\temp & bitsadmin.exe /transfer "FDT SETUP" https://raw.githubusercontent.com/fdtteknik/KundSetup/master/Client/uninstall-TV.ps1 c:\temp\uninstall-TV.ps1 & PowerShell -NoProfile -noexit -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -noexit -ExecutionPolicy Unrestricted -File ""c:\temp\uninstall-TV.ps1""' -Verb RunAs}";
