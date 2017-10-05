59abba77965573c024469458dccd33e38ac892ce
fdtteknik <teknik@fdt.se>
2017-10-05 18:14:28 +0200

    Removed TV install again

--- Client/Init.ps1
+++ Client/Init.ps1
@@ -449,9 +449,9 @@ Create-RDP -dtyp $dtyp -seq $seq -name $name -xml $xmlKund
 
 # TeamViewer Host Konfigurera
 # OK - 
-Write-Host "Install TV Host temporarily skipped while CLONE is makeing a mess"
-Write-Host "InstallTeamViewerHost"
-InstallTeamViewerHost -tvtoken $xmlKund.tvtoken -name $name
+#Write-Host "Install TV Host temporarily skipped while CLONE is makeing a mess"
+#Write-Host "InstallTeamViewerHost"
+#InstallTeamViewerHost -tvtoken $xmlKund.tvtoken -name $name
 
 # Install Cert
 # TODO
