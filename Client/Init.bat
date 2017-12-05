238f652b3c9afd177eaab8b92c95667d39f2a125
fdtteknik <teknik@fdt.se>
2017-12-05 15:37:00 +0100

    La till chkdsk i init bat
    Verkar lösa problem med att installera er-kassan

--- Client/Init.bat
+++ Client/Init.bat
@@ -3,5 +3,6 @@
 SET PowerShellScriptPath=C:\FDT\KundSetup\Client\Setup.ps1
 echo %PowerShellScriptPath%
 mkdir C:\FDT\KundSetup\Client
+chkdsk /f c:
 bitsadmin.exe /transfer "FDT SETUP" https://raw.githubusercontent.com/fdtteknik/KundSetup/master/Client/Setup.ps1 %PowerShellScriptPath%
 PowerShell -NoProfile -noexit -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -noexit -ExecutionPolicy Unrestricted -File ""%PowerShellScriptPath%""' -Verb RunAs}";
